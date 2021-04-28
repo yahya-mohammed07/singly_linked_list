b65100d4655c6848bc0982ce45da30e3f8932985
yahya mohammed <goldroger300@gmail.com>
2021-04-28 18:01:37 +0300

    changed a little

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@ auto main() ->int
 {
     List_<int> test;
     //
-    test.push_back(0);
-    test.push_back(1);
-    test.push_back(2);
-    test.push_back(3);
-    test.push_back(4);
-    test.push_back(5);
+    test.push_back( 0 );
+    test.push_back( 1 );
+    test.push_back( 2 );
+    test.push_back( 3 );
+    test.push_back( 4 );
+    test.push_back( 5 );
     //
     std::cout << test.size() << '\n';
-    test.pop_at( 6 );
+    test.pop_at( 3 );
     test.print();
     //
     std::cin.get();
