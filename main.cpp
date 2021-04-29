be6f767c5d4deb5b32d301a05ac79c0bbb4efbe5
yahya-mohammed07 <goldroger300@gmail.com>
2021-04-29 09:07:51 +0300

    updated

--- main.cpp
+++ main.cpp
@@ -2,18 +2,12 @@
 
 auto main() ->int
 {
-    List_<int> test;
+    List_<int> nums { 0, 1, 2, 3, 4, 5 };
     //
-    test.push_back(0);
-    test.push_back(1);
-    test.push_back(2);
-    test.push_back(3);
-    test.push_back(4);
-    test.push_back(5);
-    //
-    std::cout << test.size() << '\n';
-    test.pop_at( 6 );
-    test.print();
+    std::cout << nums.size() << '\n';
+    nums.pop_at(1);
+    std::cout << nums.at( nums.end()->m_next ) << '\n'; // print second data in O(1)
+    nums.print();
     //
     std::cin.get();
 }
