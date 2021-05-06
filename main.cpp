2b1c37bcdc8c97a1bb27581026829c72cd5073d7
yahya mohammed <goldroger300@gmail.com>
2021-05-06 10:35:48 +0300

    test with some STL functions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include "list.hpp"
+#include <numeric>
+#include <algorithm>
 
 auto main() ->int
 {
-    List_<int> nums { 0, 1, 2, 3, 4, 5 };
-    //
-    std::cout << nums.size() << '\n';
-    nums.pop_at(1);
-    nums.print();
-    //
-    std::cin.get();
+  List_<int> nums{3, 2, 6, -1, 3, 90, 1, 0};
+  //
+  auto sum = std::accumulate(nums.begin(), nums.end(), 0);
+  std::cout << sum << '\n';
+  for ( const auto& i : nums) { // not including head // @fixme
+    std::cout << i << ' ';
+  }
+  List_<int> temp;
+  std::iota(temp.begin(), temp.end(), 10);
 }
