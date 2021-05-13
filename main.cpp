ac6c6d7b8ecf874e045a453a5b8a057e591f46d7
yahya mohammed <goldroger300@gmail.com>
2021-05-13 16:04:06 +0300

    Test with some STD functions

--- main.cpp
+++ main.cpp
@@ -4,13 +4,18 @@
 
 auto main() ->int
 {
-  List_<int> nums{3, 2, 6, -1, 3, 90, 1, 0};
+  List_<int> n{3, 2, 6, 3, 90, 1, 0,  7};
+  List_<int> nums {std::move(n)};
+  nums.print();
   //
+  std::cout << "\n- sum: ";
   auto sum = std::accumulate(nums.begin(), nums.end(), 0);
-  std::cout << sum << '\n';
-  for (const auto& i : nums) {
-    std::cout << i << ' ';
-  }
-  List_<int> temp;
-  std::iota(temp.begin(), temp.end(), 10);
+  std::cout << sum << '\n' << "- after sorting: ";
+  nums.sort();
+  nums.print();
+  //
+  std::cout << '\n' << "- show odd values using `for_each` loop: ";
+  std::for_each(nums.begin(), nums.end(), [](auto i = 0) -> auto {
+    if ( (i & 1)  ) { std::cout << i << ' '; }
+  });
 }
