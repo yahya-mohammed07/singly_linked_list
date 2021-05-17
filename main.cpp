14af102a7e1eb9cf27d417d8e09fa11730834645
yahya mohammed <goldroger300@gmail.com>
2021-05-17 09:03:24 +0300

    Test variadic with STL functions

--- main.cpp
+++ main.cpp
@@ -4,18 +4,20 @@
 
 auto main() ->int
 {
-  List_<int> n{3, 2, 6, 3, 90, 1, 0,  7};
+  List_<int> n( 1, 2, 3, 4, 5, 6);
+  n.push_back(44,33,22,44);
+  n.push_front(-100, -190);
+  n.print();
   List_<int> nums {std::move(n)};
-  nums.print();
   //
   std::cout << "\n- sum: ";
   auto sum = std::accumulate(nums.begin(), nums.end(), 0);
   std::cout << sum << '\n' << "- after sorting: ";
-  nums.sort();
+  nums.sort(false);
   nums.print();
   //
   std::cout << '\n' << "- show odd values using `for_each` loop: ";
-  std::for_each(nums.begin(), nums.end(), [](auto i = 0) -> auto {
+  std::for_each(nums.begin(), nums.end(), [](auto i = 0) -> void {
     if ( (i & 1)  ) { std::cout << i << ' '; }
   });
 }
